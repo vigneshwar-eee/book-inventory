193fc4a086f1f16516b73c8e48d4639d8be85814
vigneshwar_m1609 <vigneshwar@softsuave.com>
2020-10-23 19:34:43 +0530

    first commit

---
+++ src/main/java/com/book/error/BookUnSupportedFieldPatchException.java
@@ -0,0 +1,11 @@
+package com.book.error;
+
+import java.util.Set;
+
+public class BookUnSupportedFieldPatchException extends RuntimeException {
+
+    public BookUnSupportedFieldPatchException(Set<String> keys) {
+        super("Field " + keys.toString() + " update is not allow.");
+    }
+
+}
